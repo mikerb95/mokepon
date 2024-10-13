2ab094fb7b40bf583daf00d6971b696454aaadb9
Michael Rodriguez <69970540+mikerb95@users.noreply.github.com>
2024-10-13 13:54:46 -0500

    agregando lógica selector personajes

---
+++ js/mokepon.js
@@ -0,0 +1,28 @@
+/* 
+La funcion iniciar juego, incluye la lógica del juego y se usa como parametro en el evento Load.
+La funcion de load, es invocar la funcion iniciar juego, una vez carga todo el HTML.
+*/
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    
+    if (inputHipodoge.checked) {
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked) {
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigueya')
+    }
+}
+
+window.addEventListener('load', iniciarJuego)
+
+
+
+
